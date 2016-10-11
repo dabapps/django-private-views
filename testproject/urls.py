076c0e78e7e75f7f65622dd13ac4c37f2f24894c
Max Hurl <max@maxhurl.co.uk>
2016-10-11 16:25:24 +0100

    Upgrade for python3 and test against more django versions

--- testproject/urls.py
+++ testproject/urls.py
@@ -1,13 +1,13 @@
-from django.conf.urls import url, patterns
+from django.conf.urls import url
 from testproject import views
 from privateviews.decorators import login_not_required
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^undecorated/$', views.undecorated),
     url(r'^test-decorator/$', login_not_required(views.undecorated)),
     url(r'^test-undecorated-class-based-view/$', views.TestClassBasedView.as_view()),
     url(r'^test-decorated-class-based-view/$', login_not_required(views.TestClassBasedView.as_view())),
     url(r'^test-public-views/$', views.test_public_views),
     url(r'^test-public-paths/$', views.test_public_paths),
-)
+]
